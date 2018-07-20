df1efc11340e954f3bf69f5ee8a0d1f7c233ff07
Sergei_Zheleznov <rusergei2010@gmail.com>
2018-07-20 09:20:51 +0300

    Added Swagger Gradle App

---
+++ Practice/SpringBootDemo1/src/main/java/com/epam/lab/SpringBootDemo1/spring/swagger/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.epam.lab.SpringBootDemo1.spring.swagger;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.epam.lab.SpringBootDemo1.controllers"))
+//                .paths(PathSelectors.ant("/v1/*"))
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfo(
+//                "Performance Tool REST API",
+//                "",
+//                "News Room ES Performance Tool",
+//                "Terms of service",
+//                new Contact("News Room", "", ""),
+//                "License of API", "API license URL");
+//    }
+//}
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
